8c1671882ce445c4383dc4bff41c6c65f079d5e2
Lance <lance.pedersen@epsb.ca>
2023-01-26 17:44:50 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+let bugVisits = 0
+basic.showNumber(bugVisits)
 basic.forever(function () {
-	
+    if (cakLandTouch.getTouch(cakLandTouch.TouchPin.P12)) {
+        bugVisits += 1
+        basic.showNumber(bugVisits)
+    }
 })
